19c3622b734e2d1a0e309bdea26fb0018718d6b8
Farzana Moury <w0737626@myscc.ca>
2020-11-18 19:50:12 -0500

    Modified Database class   * added private fields   * added required public methods (constructor, getInstance(), createTable(), and getConnection())   * supports Singleton Design Pattern

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,5 +1,57 @@
 package Database;
 
+import java.sql.*;
+
 public class Database {
+
+    private static Database instance = null;
+    private Connection connection = null;
+
+    private Database() {
+        if(connection == null){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver"); //using the new driver format
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/"
+                        + Login.DB_NAME + "?serverTimezone=UTC", Login.DB_USER, Login.DB_PASSWORD);
+                System.out.println("Connection successful");
+                //create tables
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            try {
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Database getInstance(){
+        if(instance == null){
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public void createTable(String tableName, String tableQuery, Connection connection) throws SQLException {
+        Statement createTables;
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet resultSet = metaData.getTables(Login.DB_NAME, null, tableName, null);
+
+        //will check to see if table exists in the database
+        if(resultSet.next()) {
+            System.out.println(tableName + " table already exists");
+        }
+        //otherwise we create a table
+        else{
+            createTables = connection.createStatement();
+            createTables.execute(tableQuery);
+            System.out.println(tableName + " table created");
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 
 }
