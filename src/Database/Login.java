ff13b179e85bf1fa8235bae612a74d87af70c7b0
Omar Yousef <w0753671@myscc.ca>
2020-11-18 23:08:32 -0500

    Fixed some project bugs

---
+++ src/Database/Login.java
@@ -0,0 +1,7 @@
+package Database;
+
+public class Login {
+    public static final String DB_NAME = "oyousefdb";
+    public static final String DB_USER = "oyousef";
+    public static final String DB_PASSWORD = "r35jnr35jn";
+}
