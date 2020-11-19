79121834371e468edb02b04a963b5415067dd555
Farzana Moury <w0737626@myscc.ca>
2020-11-18 21:10:25 -0500

    Modified DBConst class and Database Class   * output that tables were created weren't showing   * instantiated database by doing a db.getInstance()   * fixed the sql statements in DBConst so tables could be created   * output showed that tables were created   * added comments and javadoc comments to Database class

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -2,23 +2,42 @@ package Database;
 
 import java.sql.*;
 
+/**
+ * Database class
+ * This class will use the Singleton design pattern
+ * We do this through
+ *  - Creating a private constructor
+ *  - Create a private static instance variable
+ *  - Create a public static getInstance() method
+
+ * @author Farzana Moury
+ * @version 1.0
+ * @since Nov 18th 2020
+ */
+
 public class Database {
 
-    private static Database instance = null;
+    //properties
+    private static Database instance = null; //create an instance variable
     private Connection connection = null;
 
+    /**
+     * The private constructor which creates a connection
+     * And creates the individual tables using tableName, sql query and connection
+     */
     private Database() {
+        //setup connection
         if(connection == null){
             try{
                 Class.forName("com.mysql.cj.jdbc.Driver"); //using the new driver format
                 connection = DriverManager.getConnection("jdbc:mysql://localhost/"
                         + Login.DB_NAME + "?serverTimezone=UTC", Login.DB_USER, Login.DB_PASSWORD);
                 System.out.println("Connection successful");
-                //create tables
             }catch(Exception e){
                 e.printStackTrace();
             }
             try {
+                //create tables
                 createTable(DBConst.TABLE_NUMBER_TABLE, DBConst.CREATE_TABLE_NUMBER, connection);
                 createTable(DBConst.TABLE_GUESTS, DBConst.CREATE_TABLE_GUESTS, connection);
                 createTable(DBConst.TABLE_RESERVATIONS, DBConst.CREATE_TABLE_RESERVATIONS, connection);
@@ -28,6 +47,10 @@ public class Database {
         }
     }
 
+    /**
+     * public static method getInstance() creates an instance of Database
+     * @return Database instance
+     */
     public static Database getInstance(){
         if(instance == null){
             instance = new Database();
@@ -35,6 +58,13 @@ public class Database {
         return instance;
     }
 
+    /**
+     * public method createTable() - executes the table queries only once
+     * @param tableName
+     * @param tableQuery
+     * @param connection
+     * @throws SQLException
+     */
     public void createTable(String tableName, String tableQuery, Connection connection) throws SQLException {
         Statement createTables;
         DatabaseMetaData metaData = connection.getMetaData();
