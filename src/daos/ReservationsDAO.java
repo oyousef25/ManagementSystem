59b8aaad5493010c58c03bbc350f9d37cf065220
Omar Yousef <w0753671@myscc.ca>
2020-11-22 21:09:03 -0500

    Finished the Reservations Data Access Object (DAO Interface)

--- src/daos/ReservationsDAO.java
+++ src/daos/ReservationsDAO.java
@@ -1,4 +1,38 @@
 package daos;
 
+import pojo.Reservations;
+
+import java.util.ArrayList;
+
+/**
+ * Reservations DAO(Data Access Object) helps us:
+ * 1. access all Reservations
+ * 2. access a single Reservation
+ * 3. Create a new Reservation
+ * 4. Delete an existing Reservation
+ */
 public interface ReservationsDAO {
+    /**
+     * A functions that return all Reservations
+     * @return
+     */
+    public ArrayList<Reservations> AllReservations();
+
+    /**
+     * A function that returns a single Reservation
+     * @return
+     */
+    public Reservations getReservation(int reservationId);
+
+    /**
+     * A function that Deletes an Existing Reservation record
+     * @param id
+     */
+    public void deleteReservation(int id);
+
+    /**
+     * A function that Creates a new Reservation
+     * @param reservation
+     */
+    public void createReservation(Reservations reservation);
 }
