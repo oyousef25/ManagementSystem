7d551adcbb2bd4501ebc8bd137c0ed9f568c53de
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2020-11-24 14:53:05 -0500

    Delete Login.java

--- src/Database/Login.java
+++
@@ -1,7 +0,0 @@
-package Database;
-
-public class Login {
-    public static final String DB_NAME = "oyousefjava";
-    public static final String DB_USER = "oyousef";
-    public static final String DB_PASSWORD = "41cv441cv47pqrt7pqrt";
-}
