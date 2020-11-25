8f047381fd860f786fce874e7a16898b68a7527c
Farzana Moury <w0737626@myscc.ca>
2020-11-24 19:34:42 -0500

    Provided method definitions in TableNumTable class   * these methods include getAllTableNumbers and getTableNumber

--- src/tables/TableNumTable.java
+++ src/tables/TableNumTable.java
@@ -1,18 +1,53 @@
 package tables;
 
+import Database.DBConst;
+import Database.Database;
 import daos.TableNumDAO;
 import pojo.TableNumber;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class TableNumTable implements TableNumDAO {
+    Database db = Database.getInstance();
+    ArrayList<TableNumber> tableNumbers;
+
     @Override
     public ArrayList<TableNumber> getAllTableNumbers() {
-        return null;
+        String query = "SELECT * FROM " + DBConst.TABLE_NUMBER_TABLE;
+        try{
+            Statement getTableNumbers = db.getConnection().createStatement();
+            ResultSet data = getTableNumbers.executeQuery(query);
+
+            while(data.next()){
+                tableNumbers.add(new TableNumber(data.getInt(DBConst.TABLE_NUMBER_COLUMN_ID),
+                        data.getInt(DBConst.TABLE_NUMBER_COLUMN_NUMBER)));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return tableNumbers;
     }
 
     @Override
     public TableNumber getTableNumber(int tableId) {
+        String query = "SELECT * FROM " + DBConst.TABLE_NUMBER_TABLE+
+                " WHERE " + DBConst.TABLE_NUMBER_COLUMN_ID + " = " + tableId;
+        try{
+            Statement getTableNumbers =
+                    db.getConnection().createStatement();
+            ResultSet data = getTableNumbers.executeQuery(query);
+            if(data.next()){
+                TableNumber tableNumber =
+                        new TableNumber(data.getInt(DBConst.TABLE_NUMBER_COLUMN_ID),
+                                data.getInt(DBConst.TABLE_NUMBER_COLUMN_NUMBER));
+                return tableNumber;
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
