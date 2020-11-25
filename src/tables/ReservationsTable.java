6ac98df8d96b5f7b78a6fdfda866d6420a3321b9
Farzana Moury <w0737626@myscc.ca>
2020-11-24 20:22:16 -0500

    Provided method definitions in ReservationsTable class and modified ReservationsDAO interface   * these methods include getAllReservations and getReservation   * changed ReservationsDAO interface method AllReservations() to getAllReservations()

--- src/tables/ReservationsTable.java
+++ src/tables/ReservationsTable.java
@@ -1,19 +1,61 @@
 package tables;
 
+import Database.DBConst;
+import Database.Database;
 import daos.ReservationsDAO;
 import pojo.Reservations;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class ReservationsTable implements ReservationsDAO {
+    Database db = Database.getInstance();
+    ArrayList<Reservations> reservations;
+
     @Override
-    public ArrayList<Reservations> AllReservations() {
-        return null;
+    public ArrayList<Reservations> getAllReservations() {
+        String query = "SELECT * FROM " + DBConst.TABLE_RESERVATIONS;
+        try{
+            Statement getReservations = db.getConnection().createStatement();
+            ResultSet data = getReservations.executeQuery(query);
+
+            while(data.next()){
+                reservations.add(new Reservations(data.getInt(DBConst.RESERVATIONS_COLUMN_ID),
+                        data.getString(DBConst.RESERVATIONS_COLUMN_NAME),
+                        data.getInt(DBConst.RESERVATIONS_COLUMN_DATE),
+                        data.getInt(DBConst.RESERVATIONS_COLUMN_GUESTS),
+                        data.getInt(DBConst.RESERVATIONS_COLUMN_TABLE),
+                        data.getInt(DBConst.RESERVATIONS_COLUMN_PHONE)));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return reservations;
     }
 
     @Override
     public Reservations getReservation(int reservationId) {
-        return null;
+        String query = "SELECT * FROM " + DBConst.TABLE_RESERVATIONS+
+                " WHERE " + DBConst.RESERVATIONS_COLUMN_ID + " = " + reservationId;
+        Reservations reservation = new Reservations();
+
+        try{
+            Statement getReservations = db.getConnection().createStatement();
+            ResultSet data = getReservations.executeQuery(query);
+            data.next();
+            reservations.add(
+                    new Reservations(data.getInt(DBConst.RESERVATIONS_COLUMN_ID),
+                            data.getString(DBConst.RESERVATIONS_COLUMN_NAME),
+                            data.getInt(DBConst.RESERVATIONS_COLUMN_DATE),
+                            data.getInt(DBConst.RESERVATIONS_COLUMN_GUESTS),
+                            data.getInt(DBConst.RESERVATIONS_COLUMN_TABLE),
+                            data.getInt(DBConst.RESERVATIONS_COLUMN_PHONE)));
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reservation;
     }
 
     @Override
