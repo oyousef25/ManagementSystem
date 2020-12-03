ba99f042a3a92c9e5bea0b94e7b077e993290e54
Farzana Moury <w0737626@myscc.ca>
2020-12-03 18:41:59 -0500

    #5 Creating a Bar Graph   * created a generateChart function   * within this function added code to be able to display information   * this includes the tables and how many reservations have been done on them

--- src/panes/StatisticsPane.java
+++ src/panes/StatisticsPane.java
@@ -1,12 +1,21 @@
 package panes;
 
+import com.mysql.cj.xdevapi.Table;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import pojo.TableNumber;
+import tables.TableNumTable;
+
+import java.util.ArrayList;
 
 /**
  * Statistics Pane which is used to display information in the StatsTab
@@ -17,6 +26,8 @@ import javafx.scene.text.Text;
  * @since Dec 3rd 2020
  */
 public class StatisticsPane extends BorderPane {
+
+    private BarChart<String, Number> barGraph; //graph will be used to indicate how popular each table is
 
     /**
      * Constructor displaying Statistics content
@@ -43,5 +54,31 @@ public class StatisticsPane extends BorderPane {
 
         //setting the placement of the content VBox in this pane
         this.setTop(content);
+    }
+
+    public void generateChart(){
+        //getting access to the database
+        TableNumTable tablesTable = new TableNumTable();
+
+        //grabbing a list of table types
+        ArrayList<TableNumber> tables = tablesTable.getAllTableNumbers();
+
+        //Axes
+        NumberAxis reservations = new NumberAxis(); //indicates how many people reserved
+        CategoryAxis tableNumbers = new CategoryAxis(); //indicates the individual Tables (Table1, Table2, etc)
+        reservations.setLabel("# of Reservations");
+        tableNumbers.setLabel("Tables");
+
+
+        //populating the bar graph with information
+        barGraph = new BarChart<String, Number>(tableNumbers, reservations);
+        barGraph.setTitle("Most Used Tables");
+
+        XYChart.Series series = new XYChart.Series();
+
+        for(TableNumber table: tables){
+            series.getData().add(new XYChart.Data<String, Number>("Table " + table.getTableNum(), tablesTable.getItemCount(table.getId())));
+        }
+
     }
 }
